09f1b09f1ba7abc6d30653ad3f832f9a2fd15dd2
Dominik Stücheli - 105D Engineering <251574964+dominikstuecheli-105D-Engineering@users.noreply.github.com>
2026-08-11 21:11:30 +0200

    Removed some Image files that will not be needed for 2.0.0 and uploading files needed for testing

---
+++ BSA2/Types/HappynessFunctions/Built-in/concentrateHappyness.lua
@@ -0,0 +1,5 @@
+--Created by Dominik Stücheli on 11.08.2026
+
+function happynessFunction(student, inChoice, choiceAmount)
+	return ((inChoice-choiceAmount)/(choiceAmount-1))^2
+end
---
+++ BSA2/Types/HappynessFunctions/Built-in/linear.lua
@@ -0,0 +1,5 @@
+--Created by Dominik Stücheli on 11.08.2026
+
+function happynessFunction(student, inChoice, choiceAmount)
+	return (-inChoice+1)/(choiceAmount-1) + 1
+end
